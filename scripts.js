a794f613b2f294dc9d8c317b79f36a0a4afe04f4
Charles Buxton <chazbux@gmail.com>
2020-03-03 17:44:09 -0700

    revised button press

--- scripts.js
+++ scripts.js
@@ -9,15 +9,24 @@ document.getElementById('screen').innerHTML = x
 
 function buttonPress(value) {
   args.push(value)
-  let output = args.join('')
+  let output = Number(args.join(''))
   calcDisplay(output)
+  console.log(output)
 }
+
+
+
+
+
+
 
 function multiply() {
   let argOne = Number(args.join(''))
   document.getElementById('screen').innerHTML = " * "
   args = []
-  console.log(argOne)
+  let argTwo = Number(args.join(''))
+  result = argOne * args
+  console.log(result)
 }
 function divide() {
 
